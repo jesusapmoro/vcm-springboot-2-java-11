b7a8dc7610191a24e8c26c1ea4a97d80cd3dd4c8
jesusapmoro <jesusapmoro@gmail>
2022-04-26 20:47:01 -0300

    Project created

---
+++ src/main/java/com/jesusmoro/vcm/VcmApplication.java
@@ -0,0 +1,13 @@
+package com.jesusmoro.vcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesusmoro/vcm/VcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusmoro.vcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
